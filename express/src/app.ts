774ec6527492b81ccddc4aff040af4b3bb3de207
ohtaeg <otk1090@naver.com>
2023-12-05 01:18:45 +0900

    클래스화

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,44 +1,67 @@
 // https://expressjs.com/en/starter/hello-world.html
 
 import * as express from "express";
-import { Cat } from "./cats/cats.model";
 import catsRouter from "./cats/cats.router";
 
-const app: express.Application = express();
-const port: number = 8080;
+class Server {
+  public app: express.Application;
+  private port: number;
 
-/**
- * 중간에서, 개발자가 원하는 무언가를 할 수 있도록 하는 것, express 에서는 next() 가 있는 경우를 middleware 라고 한다.
- * 미들웨어는 순서가 중요하다.
- * 만약 미들웨어가 함수들 아래에 위치하게 되면 호출되지 않는다. express가 endpoint를 찾기 위해 위에서 top - bottom으로 찾기 때문
- * 그래서 라우터마다 미들웨어를 각각 다르게 적용하고 싶으면 중간중간 삽입한다.
- */
-app.use((req, res, next) => {
-  console.log("logging middle ware");
-  next();
-  console.log(res.sendDate);
-});
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+    this.port = 8080;
+  }
 
-// json middle ware
-app.use(express.json());
+  private setRouter() {
+    // cats 라우터를 등록해준다.
+    this.app.use(catsRouter);
+  }
 
-// cats 라우터를 등록해준다.
-app.use(catsRouter);
+  private setMiddleware() {
+    /**
+     * 중간에서, 개발자가 원하는 무언가를 할 수 있도록 하는 것, express 에서는 next() 가 있는 경우를 middleware 라고 한다.
+     * 미들웨어는 순서가 중요하다.
+     * 만약 미들웨어가 함수들 아래에 위치하게 되면 호출되지 않는다. express가 endpoint를 찾기 위해 위에서 top - bottom으로 찾기 때문
+     * 그래서 라우터마다 미들웨어를 각각 다르게 적용하고 싶으면 중간중간 삽입한다.
+     */
+    this.app.use((req, res, next) => {
+      console.log("logging middle ware");
+      next();
+      console.log(res.sendDate);
+    });
 
-// 이것도 미들웨어이다. app.get("/cats/som")에 대한 미들웨어임
-app.get("/cats/som", (req, res, next: express.NextFunction) => {
-  console.log("only som middle ware");
-  next();
-});
+    // json middle ware
+    this.app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    this.setRouter();
 
-/**
- * 에러 핸들링 미들 웨어
- */
-app.use((req, res, next) => {
-  console.log("error middle ware");
-  res.send({ error: "404 not found" });
-});
+    // 이것도 미들웨어이다. app.get("/cats/som")에 대한 미들웨어임
+    this.app.get("/cats/som", (req, res, next: express.NextFunction) => {
+      console.log("only som middle ware");
+      next();
+    });
+
+    /**
+     * 에러 핸들링 미들 웨어
+     */
+    this.app.use((req, res, next) => {
+      console.log("error middle ware");
+      res.send({ error: "404 not found" });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(this.port, () => {
+      console.log(`Example app listening on port ${this.port}`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
